efdea06c7ddf8fed0851aa5df2d2db61dc954af6
devansh-khandelwal <devanshk2107@gmail.com>
2023-05-21 13:04:01 +0530

    completed About page

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,31 +3,29 @@ import { Nav, NavLink, Bars, NavMenu } from "./NavbarElements";
 
 const Navbar = () => {
   return (
-    <>
-      <Nav>
-        <Bars />
-        <NavMenu>
-          <NavLink to="/home" activeStyle>
-            Home
-          </NavLink>
-          <NavLink to="/about" activeStyle>
-            About Camp
-          </NavLink>
-          <NavLink to="/tents" activeStyle>
-            Our Tents
-          </NavLink>
-          <NavLink to="/activities" activeStyle>
-            Activities
-          </NavLink>
-          <NavLink to="/gallery" activeStyle>
-            Gallery/Museum
-          </NavLink>
-          <NavLink to="/contact" activeStyle>
-            Contact Us
-          </NavLink>
-        </NavMenu>
-      </Nav>
-    </>
+    <Nav>
+      <Bars />
+      <NavMenu>
+        <NavLink to="/home" activeStyle>
+          Home
+        </NavLink>
+        <NavLink to="/about" activeStyle>
+          About Camp
+        </NavLink>
+        <NavLink to="/tents" activeStyle>
+          Our Tents
+        </NavLink>
+        <NavLink to="/activities" activeStyle>
+          Activities
+        </NavLink>
+        <NavLink to="/gallery" activeStyle>
+          Gallery/Museum
+        </NavLink>
+        <NavLink to="/contact" activeStyle>
+          Contact Us
+        </NavLink>
+      </NavMenu>
+    </Nav>
   );
 };
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,54 +1,35 @@
 import React from "react";
 import "../styles/About.css";
+import img from "../assets/img1.jpeg";
+import firstParaImg from "../assets/img2.jpeg";
+
+const firstPara =
+  "Major's Camp is spread in 100 Acres.The entire premises is Army themed and each spot at Major's Camp narrates a warrior's / Army's story." +
+  "It is surrounded by 400 acres of nature and greenery. It has a beautiful mountain view alongside a river." +
+  "There is crop vegetation of a number of fruits & vegetables (apple, mango, guava, papaya, watermelon, muskmelon," +
+  "dragon fruit, banana, strawberry, grapes, potato, tomato, onion, cucumber, radish, carrot, brinjal and so much more)." +
+  "It has both cylcling and trekking tracks for you to exercise." +
+  "All tents are camouflaged. There is a barbeque area, briefing area for team games, dining area, Amar Jyoti." +
+  "The entire camp is pet friendly.";
 
 const About = () => {
   return (
-    <>
-      <div>
-        <h3>1.The entire Major's Camp is spread in 100 Acres.</h3>
-        <h3>
-          2. The entire premises is Army Themed and each spot at Major's Camp
-          narrates a warrior's / Army Story.
-        </h3>
-        <h3>3. It is surrounded by 400 acres of nature and greenery.</h3>
-        <h3>4. Beautiful Mountain View</h3>
-        <h3>5. River View along side</h3>
-        <h3>
-          6. Crop Vegetation of fruits & Vegetables (apple, mango, guava,
-          papaya, watermelon, muskmelon, dragon fruit, banana, strawberry,
-          grapes, potato, tomato, onion, cucumber, radish, carrot, brinjal and
-          so much more)
-        </h3>
-        <h3>7. Trekking Track, Cycling Track</h3>
-        <h3>8. All tents are camouflaged</h3>
-        <h3>9. Barbeque Area</h3>
-        <h3>10. Briefing Area for Team Games</h3>
-        <h3>11. Dinning Area</h3>
-        <h3>12..AMAR Jyoti</h3>
-        <h3>13. PET Friendly</h3>
+    <div className="about">
+      <div className="aboutImage">
+        <img src={img} alt="" />
       </div>
-      <div className="buttons">
-        <div className="activitiesButton">
-          <a href="/activities">
-            <button style={{ width: "100px", height: "50px" }}>
-              Activities at our Premises
-            </button>
-          </a>
+      <div className="heading">
+        <h1>About Camp</h1>
+      </div>
+      <div className="firstParagraph">
+        <div className="image">
+          <img src={firstParaImg} alt="" />
         </div>
-        <div className="picturesButton">
-          <a href="/gallery">
-            <button style={{ width: "100px", height: "50px" }}>
-              Pictures of Premises
-            </button>
-          </a>
-        </div>
-        <div className="bookNowButton">
-          <a href="/tents">
-            <button style={{ width: "100px", height: "50px" }}>Book Now</button>
-          </a>
+        <div className="text">
+          <p>{firstPara}</p>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
