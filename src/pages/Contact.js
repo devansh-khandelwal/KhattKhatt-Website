7632aee94021baa88cfa4550334c2e9d86c899ff
devansh-khandelwal <devanshk2107@gmail.com>
2023-05-21 17:50:35 +0530

    Contact page completed

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,33 +1,51 @@
 import React from "react";
 import "../styles/Contact.css";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import EmailIcon from "@mui/icons-material/Email";
+import LocalPhoneRoundedIcon from "@mui/icons-material/LocalPhoneRounded";
+import { Link } from "@mui/material";
 
 const Contact = () => {
   return (
     <div className="contact">
-      <div className="contactTitle">
-        <h1>Contact Details</h1>
-      </div>
-      <div className="contactDetails">
+      <h1>Contact Us</h1>
+      <div className="body">
+        <div className="address">
+          <div className="icon">
+            <LocationOnIcon sx={{ color: "#0047AB" }} />
+          </div>
+          <div className="subHeading">Address</div>
+          <div className="text">
+            234, 2nd Cross, Kundahalli,
+            <br />
+            ITPL main Road, Bangalore 560037
+          </div>
+        </div>
         <div className="email">
-          Email ID -{" "}
-          <span>
-            <a
-              href="customercare@majorscamp.com Booking@majorscamp.com
-        Help@majorscamp.com"
-            >
-              customercare@majorscamp.com Booking@majorscamp.com
-              Help@majorscamp.com
-            </a>
-          </span>
+          <div className="icon">
+            <EmailIcon sx={{ color: "#0047AB" }} />
+          </div>
+          <div className="subHeading">Email</div>
+          <div className="text">
+            <Link href="#" variant="body2">
+              {"customercare@majorscamp.com "}
+              <br />
+              {"Booking@majorscamp.com"}
+              <br />
+              {"Help@majorscamp.com"}
+            </Link>
+          </div>
         </div>
-        <div className="phoneNumber">
-          Phone Number- <span>+91 6362147935 , + 91 9155607067</span>
-        </div>
-        <div className="linkToVenue">
-          Link to Venue-{" "}
-          <span>
-            <a href="/">Link</a>
-          </span>
+        <div className="phone">
+          <div className="icon">
+            <LocalPhoneRoundedIcon sx={{ color: "#0047AB" }} />
+          </div>
+          <div className="subHeading">Phone</div>
+          <div className="text">
+            +91 6362147935
+            <br />
+            +91 9155607067
+          </div>
         </div>
       </div>
     </div>
