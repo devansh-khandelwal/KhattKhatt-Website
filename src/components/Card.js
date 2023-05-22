ad553565d9d37a0d9765dcff9ce7d8b09da50ef7
devansh-khandelwal <devanshk2107@gmail.com>
2023-05-22 09:06:38 +0530

    Activities Page done

---
+++ src/components/Card.js
@@ -0,0 +1,24 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+
+export default function ActionAreaCard({ img, title, text }) {
+  return (
+    <Card sx={{}} style={{ margin: "15px" }}>
+      <CardActionArea>
+        <CardMedia component="img" height="200" image={img} />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {text}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
--- src/pages/Activities.js
+++ src/pages/Activities.js
@@ -1,43 +1,98 @@
 import React from "react";
 import "../styles/Activities.css";
+import ActionAreaCard from "../components/Card";
 
 const Activities = () => {
+  const cardData = [
+    {
+      title: "Army Themed Games",
+      img: "https://i.pinimg.com/originals/43/aa/e2/43aae200b08373696812d86175d844ff.png",
+      text: "(Treasure Hunt, Leadership game, Team Spirit, Battlefieldwar games, etc)",
+    },
+    {
+      title: "Boat Ride with Floating Snacks",
+      img: "https://media.tacdn.com/media/attractions-splice-spp-674x446/0c/09/20/cd.jpg",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste, repellendus.",
+    },
+    {
+      title: "Cycling",
+      img: "https://images.moneycontrol.com/static-mcnews/2021/10/coen-van-de-broek-On4QWhHJCEM-unsplash-1-770x433.jpg?impolicy=website&width=770&height=431",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste, repellendus",
+    },
+    {
+      title: "ATV",
+      img: "https://powersports.honda.com/-/media/products/segment/atv/atv-assets/fourtrax-foreman-rubicon-4x4-750x750.jpg",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste, repellendus",
+    },
+    {
+      title: "Army Themed Photo Zone",
+      img: "https://im.whatshot.in/img/2020/Jan/battalioonog-1580012601.jpg",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste, repellendus",
+    },
+    {
+      title: "Barbeque your food",
+      img: "https://images.pexels.com/photos/2233729/pexels-photo-2233729.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste, repellendus",
+    },
+    {
+      title: "Crop Tour",
+      img: "https://www.dtnpf.com/mydtn-public-core-portlet/servlet/GetStoredImage?category=CMS&symbolicName=wheat-tour-joel-reichenberger-img_9525.jpg&width=696",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste, repellendus",
+    },
+    {
+      title: "Site Seeing on Jeep",
+      img: "https://images.thrillophilia.com/image/upload/s--JdeYuJKr--/c_fill,h_775,q_auto,w_1600/f_auto,fl_strip_profile/v1/images/photos/000/141/945/original/1548405028_shutterstock_1083558866.jpg.jpg?1548405028",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste, repellendus",
+    },
+    {
+      title: "Pluck your own Vegetable and get it cooked by us.",
+      img: "https://www.foodandwine.com/thmb/QpxHuBw1ACzW_G9TBNhuOUdxnqI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/the-best-way-to-store-vegetables-to-keep-them-fresh-as-long-as-possible-FT-BLOG0320-2-d92cc1b173504702ba0bf5cdaec82c8a.jpg",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste, repellendus",
+    },
+    {
+      title: "Light & Sound Show",
+      img: "https://haryanatourism.gov.in/WriteReadData/mediafiles/image/jyotisar_pic11.jpg",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste, repellendus",
+    },
+    {
+      title: "Army Themed Museum",
+      img: "https://previews.123rf.com/images/lestertair/lestertair1708/lestertair170801077/85151528-budapest-hungary-22-august-2017-museum-of-the-history-of-the-military-of-the-hungarian-army-with.jpg",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste, repellendus",
+    },
+    {
+      title: "Dress like a Soldier",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxpEkXUHuJGD14CN4j4bxAULPlTlUL-obQW_dImayFew&usqp=CAU&ec=48665699",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste, repellendus",
+    },
+    {
+      title: "AMAR Jyoti",
+      img: "https://media.istockphoto.com/id/802504394/photo/amar-jawan-jyoti-in-jallianwala-bagh-memorial-in-amritsar-india.jpg?s=612x612&w=0&k=20&c=4yY1-HGX_XoewTlsUwVaRBg7ObVeVjf-3xT5JG_yaEE=",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste, repellendus",
+    },
+  ];
+
+  const rows = [];
+  for (let i = 0; i < cardData.length; i += 3) {
+    rows.push(cardData.slice(i, i + 3));
+  }
+
   return (
     <div className="activities">
-      <div className="activitiesTitle">
-        <h2>Activities at Major's Camp</h2>
-      </div>
-      <ol>
-        <li>
-          Army Themed Games: (Treasure Hunt, Leadership game, Team Spirit,
-          Battlefieldwar games etc (Included)
-        </li>
-        <li>Boat Ride with Floating Snacks (Optional)</li>
-        <li>Cycling (Optional)</li>
-        <li>ATV (Optional)</li>
-        <li>Army Themed Photo Zone (Included)</li>
-        <li>Barbeque your food (Included)</li>
-        <li>Crop Tour (Included)</li>
-        <li>Site Seeing on Jeep (Optional)</li>
-        <li>Pluck your own Vegetable and get it cooked by us.(Optional)</li>
-        <li>Light & Sound Show</li>
-        <li>Army Themed Museum(Included)</li>
-        <li>Dress like a Soldier (Optional)</li>
-        <li>AMAR Jyoti</li>
-      </ol>
-      <div className="activitiesButtons">
-        <div className="bookNowButton">
-          <a href="/tents">
-            <button style={{ width: "100px", height: "50px" }}>Book Now</button>
-          </a>
-        </div>
-        <div className="photosButton">
-          <a href="/">
-            <button style={{ width: "100px", height: "50px" }}>
-              Click for Photos
-            </button>
-          </a>
-        </div>
+      <h1>Activities</h1>
+      <div className="body">
+        {rows.map((row, rowIndex) => (
+          <div key={rowIndex} className="row">
+            {row.map((card, cardIndex) => (
+              <div key={cardIndex} className="card-wrapper">
+                <ActionAreaCard
+                  title={card.title}
+                  img={card.img}
+                  text={card.text}
+                />
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   );
