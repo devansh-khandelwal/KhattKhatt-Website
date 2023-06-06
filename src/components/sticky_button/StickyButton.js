a2f9835cd69c2fa8f187ad06549b5257ef30cb6f
devansh-khandelwal <devanshk2107@gmail.com>
2023-06-06 22:39:47 +0530

    updated sticky button

--- src/components/sticky_button/StickyButton.js
+++ src/components/sticky_button/StickyButton.js
@@ -1,28 +1,13 @@
 import React from "react";
 import "./StcikyButton.css";
-import { CardActions, Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import img from "../../assets/logo/logo2.png";
 
 const StickyButton = () => {
-  const navigate = useNavigate();
-
-  const onClick = () => {
-    navigate("/tents");
-  };
-
   return (
-    <div className="stickyButton">
-      <CardActions sx={{ justifyContent: "center" }}>
-        <Button
-          size="large"
-          color="primary"
-          style={{ margin: "0 auto", display: "flex" }}
-          variant="contained"
-          onClick={onClick}
-        >
-          Book Now
-        </Button>
-      </CardActions>
+    <div>
+      <a href="/tents" className="stickyButton">
+        <img src={img} alt="my_image" />
+      </a>
     </div>
   );
 };
